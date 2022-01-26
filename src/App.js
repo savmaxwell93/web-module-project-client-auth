eb358e42fd908668a7de7212aabfc7255f6bf557
Savannah <savmaxwell@gmail.com>
2022-01-25 17:32:39 -0700

    built basic components and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>LOGIN</h2>
+    </div>
+  )
+}
+
+const FriendsList = () => {
+  return (
+    <div>
+      <h2>FRIENDS LIST</h2>
+    </div>
+  )
+}
+
+const AddFriends = () => {
+  return (
+    <div>
+      <h2>ADD FRIEND</h2>
+    </div>
+  )
+}
+
+const Logout = () => {
+  return (
+    <div></div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/friends/add'>
+            <AddFriends/>
+          </Route>
+          <Route exact path='/friends'>
+            <FriendsList/>
+          </Route>
+          <Route path='/logout'>
+            <Logout/>
+          </Route>
+          <Route exact path='/login'>
+            <Redirect to='/'/>
+          </Route>
+          <Route exact path='/'>
+            <Login/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
